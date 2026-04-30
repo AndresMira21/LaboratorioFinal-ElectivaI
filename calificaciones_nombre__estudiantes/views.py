535a63d8b013ef0bf07a0975eabad0aff501b555
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-04-30 11:51:35 -0500

    creada estructura del form

--- calificaciones_nombre__estudiantes/views.py
+++ calificaciones_nombre__estudiantes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .forms import CalificacionForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def crear_calificacion(request):
+    if request.method == 'POST':
+        form = CalificacionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crear_calificacion')
+    else:
+        form = CalificacionForm()
+
+    return render(request, 'calificaciones/crear.html', {'form': form})
